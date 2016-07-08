c9fb78b14ec679becf5e32612d6cfc935ecd6f85
Boris Smus <boris@smus.com>
2016-07-07 18:16:12 -0700

    Constants for message passing.

---
+++ src/message.js
@@ -0,0 +1,11 @@
+/**
+ * Messages from the API to the embed.
+ */
+var Message = {
+  PLAY: 'play',
+  PAUSE: 'pause',
+  ADD_HOTSPOT: 'addhotspot',
+  DEVICE_MOTION: 'devicemotion',
+};
+
+module.exports = Message;
