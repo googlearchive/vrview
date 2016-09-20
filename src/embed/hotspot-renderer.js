ed52193645f0112949edc60e8bf8b06cc489fadf
Boris Smus <boris@smus.com>
2016-09-19 17:26:54 -0700

    fixed hotspots on iOS

--- src/embed/hotspot-renderer.js
+++ src/embed/hotspot-renderer.js
@@ -47,15 +47,18 @@ function HotspotRenderer(worldRenderer) {
   this.worldRenderer = worldRenderer;
   this.scene = worldRenderer.scene;
 
+  // Note: this event must be added to document.body and not to window for it to
+  // work inside iOS iframes.
+  var body = document.body;
   // Bind events for hotspot interaction.
   if (!Util.isMobile()) {
     // Only enable mouse events on desktop.
-    window.addEventListener('mousedown', this.onMouseDown_.bind(this));
-    window.addEventListener('mousemove', this.onMouseMove_.bind(this));
-    window.addEventListener('mouseup', this.onMouseUp_.bind(this));
+    body.addEventListener('mousedown', this.onMouseDown_.bind(this), false);
+    body.addEventListener('mousemove', this.onMouseMove_.bind(this), false);
+    body.addEventListener('mouseup', this.onMouseUp_.bind(this), false);
   }
-  window.addEventListener('touchstart', this.onTouchStart_.bind(this));
-  window.addEventListener('touchend', this.onTouchEnd_.bind(this));
+  body.addEventListener('touchstart', this.onTouchStart_.bind(this), false);
+  body.addEventListener('touchend', this.onTouchEnd_.bind(this), false);
 
   // Add a placeholder for hotspots.
   this.hotspotRoot = new THREE.Object3D();
